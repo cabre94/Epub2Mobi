b3d1eff3fded6e6acbd0b904fafd0e12c626cdd8
Cabre <cabre94@hotmail.com>
2020-05-31 20:55:23 -0300

    Pequeñas modificaciones de belleza en el codigo

--- epub2mobi.py
+++ epub2mobi.py
@@ -2,7 +2,7 @@
 File: epub2mobi.py
 Author: Facundo Martin Cabrera
 Email: cabre94@hotmail.com
-GitLab: https://github.com/Cabre1994
+GitLab: https://github.com/cabre94
 Description:
     Script para convertir libros .epub a .mobi. Es para Linux y hay que tener instalado Calibre
 """
@@ -11,7 +11,7 @@ import os
 path = os.getcwd()
 
 # Crea el directorio para guardar
-save_path = os.path.join(path,'MOBI')
+save_path = os.path.join(path, 'MOBI')
 
 # Carpetas
 folders = os.listdir(path)
@@ -21,13 +21,13 @@ if not os.path.exists(save_path):
 
 for f in folders:
 	if os.path.isdir(f):
-		f_save = os.path.join(save_path,f)
+		f_save = os.path.join(save_path, f)
 		if not os.path.exists(f_save):
 			os.makedirs(f_save)
 
 		books = os.listdir(os.path.join(path, f))
 		for b in books:
 			mobi = b.replace('epub', 'mobi')
-			if not os.path.exists(os.path.join(save_path, f, mobi )):
+			if not os.path.exists(os.path.join(save_path, f, mobi)):
 				os.system('ebook-convert ' + os.path.join(path, f, b) + ' ' + os.path.join(save_path, f, mobi))
 			print("\nSe convirtio {}\n".format(mobi))
